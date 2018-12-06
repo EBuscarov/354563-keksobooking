720c741e39331af0205dec34acc635c001d1ae7d
EBuscarov <EBuscarov@gmail.com>
2018-12-06 20:56:29 +0900

    Изменил массив из 8 объектов

--- js/map.js
+++ js/map.js
@@ -1,8 +1,6 @@
 'use strict';
 
 var TITLE = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
-var LOCATION_X = 600;
-var LOCATION_Y = 350;
 var MIN_PRICE = 1000;
 var MAX_PRICE = 1000000;
 var TYPE = ['palace', 'flat', 'house', 'bungalo'];
@@ -12,6 +10,15 @@ var CHECKIN = ['12:00', '13:00', '14:00'];
 var CHECKOUT = ['12:00', '13:00', '14:00'];
 var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+var MIN_X = 0;
+var MAX_X = 1200;
+var MIN_Y = 130;
+var MAX_Y = 630;
+
+var avatar = [];
+for (var i = 1; i < 9; i++) {
+  avatar.push('img/avatar/user0' + i + '.png');
+}
 
 var randomPrice = function () {
   return Math.floor(Math.random() * (MAX_PRICE - MIN_PRICE + 1) + MIN_PRICE);
@@ -21,32 +28,44 @@ var randomRooms = function () {
   return Math.floor(Math.random() * (MAX_ROOMS - MIN_ROOMS + 1) + MIN_ROOMS);
 };
 
-var randomGuest = randomRooms;
+var randomGuests = function () {
+  return Math.floor(Math.random() * (randomRooms() - MIN_ROOMS + 1) + MIN_ROOMS);
+};
 
-var ads = [];
+var locationX = function () {
+  return Math.floor(Math.random() * (MAX_X - MIN_X + 1) + MIN_X);
+};
 
-var author = {};
-author.avatar = avatar;
-var avatar = [];
-for (var i = 1; i < 9; i++) {
-  avatar.push('img/avatar/user0' + i + '.png');
+var locationY = function () {
+  return Math.floor(Math.random() * (MAX_Y - MIN_Y + 1) + MIN_Y);
+};
+
+var addressXY = [];
+for (i = 1; i < 9; i++) {
+  addressXY.push({
+    x: locationX(),
+    y: locationY()
+  });
 }
 
-var offer = {};
-offer.title = TITLE;
-offer.address = [LOCATION_X, LOCATION_Y];
-offer.price = randomPrice(MIN_PRICE, MAX_PRICE);
-offer.type = TYPE;
-offer.room = randomRooms(MIN_ROOMS, MAX_ROOMS);
-offer.guest = randomGuest;
-offer.checkin = CHECKIN;
-offer.checkout = CHECKOUT;
-offer.features = FEATURES;
-offer.description = null;
-offer.photos = PHOTOS;
-
-var location = {};
-location.x = LOCATION_X;
-location.y = LOCATION_Y;
-
-ads.push(author, offer, location);
+// Массив из 8 объектов размещения(объявлений)
+var ads = [];
+for (i = 0; i < 8; i++) {
+  ads.push({
+    avatar: avatar[i],
+    title: TITLE[i],
+    address: addressXY[i],
+    price: randomPrice(),
+    rooms: randomRooms(),
+    guests: randomGuests(),
+    type: TYPE[Math.floor(Math.random() * TYPE.length)],
+    checkin: CHECKIN[Math.floor(Math.random() * CHECKIN.length)],
+    // checkout: ads[i].checkin,
+    checkout: CHECKOUT[Math.floor(Math.random() * CHECKOUT.length)],
+    features: FEATURES,
+    description: null,
+    photos: PHOTOS,
+    x: locationX(),
+    y: locationY()
+  });
+}
