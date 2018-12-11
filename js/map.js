dc6ee4792dbef849d91f5f54e6ebaadc32def703
EBuscarov <EBuscarov@gmail.com>
2018-12-12 00:25:04 +0900

    Изменил наполненеи массива из 8 объектов. Добавил вывод пинов используюя DocumentFragment без изменения координат, аватарки и комментария к картинке

--- js/map.js
+++ js/map.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var OBJECTS = 8;
 var TITLE = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
 var MIN_PRICE = 1000;
 var MAX_PRICE = 1000000;
@@ -14,11 +15,6 @@ var MIN_X = 0;
 var MAX_X = 1200;
 var MIN_Y = 130;
 var MAX_Y = 630;
-
-var avatar = [];
-for (var i = 1; i < 9; i++) {
-  avatar.push('img/avatar/user0' + i + '.png');
-}
 
 var randomPrice = function () {
   return Math.floor(Math.random() * (MAX_PRICE - MIN_PRICE + 1) + MIN_PRICE);
@@ -40,36 +36,57 @@ var locationY = function () {
   return Math.floor(Math.random() * (MAX_Y - MIN_Y + 1) + MIN_Y);
 };
 
-var addressXY = [];
-for (i = 1; i < 9; i++) {
-  addressXY.push({
-    x: locationX(),
-    y: locationY()
-  });
-}
-
-// Массив из 8 объектов размещения(объявлений)
+// Массив из 8 объектов
 var ads = [];
-for (i = 0; i < 8; i++) {
+for (var i = 0; i < OBJECTS; i++) {
   ads.push({
-    avatar: avatar[i],
-    title: TITLE[i],
-    address: addressXY[i],
-    price: randomPrice(),
-    rooms: randomRooms(),
-    guests: randomGuests(),
-    type: TYPE[Math.floor(Math.random() * TYPE.length)],
-    checkin: CHECKIN[Math.floor(Math.random() * CHECKIN.length)],
-    // checkout: ads[i].checkin,
-    checkout: CHECKOUT[Math.floor(Math.random() * CHECKOUT.length)],
-    features: FEATURES,
-    description: null,
-    photos: PHOTOS,
-    x: locationX(),
-    y: locationY()
+    author: {
+      avatar: 'img/avatar/user0' + i + '.png'
+    },
+    offer: {
+      title: TITLE[Math.floor(Math.random() * TITLE.length)],
+      address: location.x + ', ' + location.y,
+      price: randomPrice(),
+      rooms: randomRooms(),
+      guests: randomGuests(),
+      type: TYPE[Math.floor(Math.random() * TYPE.length)],
+      checkin: CHECKIN[Math.floor(Math.random() * CHECKIN.length)],
+      checkout: CHECKOUT[Math.floor(Math.random() * CHECKOUT.length)],
+      features: FEATURES,
+      description: null,
+      photos: PHOTOS,
+    },
+    location: {
+      x: locationX(),
+      y: locationY()
+    }
   });
+  ads[i].offer.address = ads[i].location.x + ', ' + ads[i].location.y;
 }
 
 // Удаляем класс .map--faded
 var switchMap = document.querySelector('.map');
 switchMap.classList.remove('map--faded');
+
+// Создаем метки на карте
+var pinListElement = document.querySelector('.map__pins');
+var pinMapTemplate = document.querySelector('#pin')
+    .content
+    .querySelector('.map__pin');
+
+var renderPin = function () {
+  var pinElement = pinMapTemplate.cloneNode(true);
+
+  pinElement.style.left = location.x + 'px';
+  pinElement.style.top = location.y + 'px';
+  pinElement.src = ads[i].author.avatar[i];
+  pinElement.alt = ads[i].offer.title;
+
+  return pinElement;
+};
+
+var fragment = document.createDocumentFragment();
+for (i = 0; i < ads.length; i++) {
+  fragment.appendChild(renderPin(ads[i]));
+}
+pinListElement.appendChild(fragment);
